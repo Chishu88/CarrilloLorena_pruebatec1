e4ed36dd6a1ddc238a5184e75431deb4706b2307
lorena-carrillo-trillo <trillo88gmail.com>
2024-01-02 11:32:28 +0100

    .

--- Empleados.sql
+++ Empleados.sql
@@ -12,3 +12,17 @@ INSERT INTO `` (`EMPLEADOID`,`CARGO`,`FECHAINICIO`,`NOMBRE`,`SALARIO`) VALUES (5
 INSERT INTO `` (`EMPLEADOID`,`CARGO`,`FECHAINICIO`,`NOMBRE`,`SALARIO`) VALUES (6,'Comercial','2020-04-04','Eloisa',1300);
 INSERT INTO `` (`EMPLEADOID`,`CARGO`,`FECHAINICIO`,`NOMBRE`,`SALARIO`) VALUES (7,'Programador','2023-05-01','Adriana',1400);
 INSERT INTO `` (`EMPLEADOID`,`CARGO`,`FECHAINICIO`,`NOMBRE`,`SALARIO`) VALUES (9,'RR.HH','2021-05-10','Laura',1200);
+
+
+use empleados;
+select * from empleado;
+
+UPDATE empleado
+SET nombre = 'Lorena', salario = 1.650
+WHERE empleadoId = 1;
+
+DELETE FROM empleado
+WHERE empleadoId = 9;
+
+SELECT * FROM empleado
+WHERE cargo = 'Programador';
